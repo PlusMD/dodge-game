fa091f0ff47911a190b671892fb1c78207c4e1fd
Daniel Dennis <danmden196@gmail.com>
2017-09-01 12:12:43 +0100

    Added random block spawner
    Blocks now spawn randomly, however only spawn once.

---
+++ dodge-game/Assets/Spawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+
+    public Transform[] spawnPoints;
+
+    public GameObject blockPrefab; 
+
+	void Start ()
+	{
+
+	    int randomIndex = Random.Range(0, spawnPoints.Length);
+
+	    for (int i = 0; i < spawnPoints.Length; i++)
+	    {
+	        if (randomIndex != i)
+	        {
+	            Instantiate(blockPrefab, spawnPoints[i].position, Quaternion.identity); 
+	        }
+	    }
+
+	}
+
+}

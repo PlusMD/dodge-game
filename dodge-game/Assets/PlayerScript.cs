293e9ca4a9f5e0d48eed7f28584e7a1846d6206f
Daniel Dennis <danmden196@gmail.com>
2017-09-01 11:40:51 +0100

    Movement and BG
    Added simple player movement with smoothing.
    
    Background is set.

---
+++ dodge-game/Assets/PlayerScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    //Speed
+    public float speed = 15f;
+
+    //Rigibody access 
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        //Finds rigidbody and calls it rb 
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        float x = Input.GetAxis("Horizontal")* Time.fixedDeltaTime * speed; //GetAxisRaw for no smoothing. 
+
+        rb.MovePosition(rb.position + Vector2.right * x); //Vector2.right 0y 1x *x = movement
+
+
+    } 
+}
